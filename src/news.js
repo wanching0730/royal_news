112b468766cf7067d20e2259fde7fa6af0e9e44a
wanching_97 <wanching_97@hotmail.com>
2018-05-06 20:12:36 +0800

    added news.js

--- src/news.js
+++ src/news.js
@@ -1,10 +1,5 @@
-const url =
-  'https://newsapi.org/v2/top-headlines?country=us&apiKey=79af8a0825ba4443adf9c1f76f8913cb';
+const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5422f467518b41208d7640102ef9014a";
 
 export async function getNews() {
-  try { 
-    return await fetch(url).then(respones => response.json()).articles;
-  } catch (error) {
-    throw error;
-  }
+    return fetch(url).then(response => response.json()).then(json => json.articles);
 }
