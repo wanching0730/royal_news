6a43d3c19b75308558682b4c23c5a799199e51de
wanching_97 <wanching_97@hotmail.com>
2018-05-06 20:14:15 +0800

    added App.js

--- App.js
+++ App.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import { FlatList } from 'react-native';
 
-// Import getNews function from news.js
 import { getNews } from './src/news';
 import Article from './src/components/Article';
 
 export default class App extends React.Component {
+
   constructor(props) {
     super(props);
+
     this.state = { articles: [], refreshing: true };
     this.fetchNews = this.fetchNews.bind(this);
   }
 
+  // called after a component is mounted
   componentDidMount() {
     this.fetchNews();
   }
 
   fetchNews() {
-    getNews()
-      .then(articles => this.setState({ articles, refreshing: false }))
-      .catch(() => this.setState({ refreshing: false }));
+    getNews().then(articles => this.setState({ articles, refreshing: false }))
+    .catch(() => this.state({ refreshing: false}));
   }
 
   handleRefresh() {
-    this.setState(
+    this.setState( 
       {
         refreshing: true
-      },
+      }, 
       () => this.fetchNews()
     );
   }
@@ -35,11 +36,17 @@ export default class App extends React.Component {
     return (
       <FlatList
         data={this.state.articles}
+        // pass article to Article component
         renderItem={({ item }) => <Article article={item} />}
         keyExtractor={item => item.url}
+
+        // represent the spinner animation
         refreshing={this.state.refreshing}
+
+        // called immediately when refreshing is true
         onRefresh={this.handleRefresh.bind(this)}
       />
     );
   }
 }
+
