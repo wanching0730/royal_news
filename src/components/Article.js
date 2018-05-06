3f9395e65f7c0b77094d3c2ff0ec213db1477f3b
wanching_97 <wanching_97@hotmail.com>
2018-05-06 20:14:43 +0800

    added Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,59 +4,62 @@ import { Text, Button, Card, Divider } from 'react-native-elements';
 import moment from 'moment';
 
 export default class Article extends React.Component {
-  render() {
-    const {
-      title,
-      description,
-      publishedAt,
-      source,
-      urlToImage,
-      url
-    } = this.props.article;
-    const { noteStyle, featuredTitleStyle } = styles;
-    const time = moment(publishedAt || moment.now()).fromNow();
-    const defaultImg =
-      'https://wallpaper.wiki/wp-content/uploads/2017/04/wallpaper.wiki-Images-HD-Diamond-Pattern-PIC-WPB009691.jpg';
+    render() {
 
-    return (
-      <TouchableNativeFeedback
-        useForeground
-        onPress={() => Linking.openURL(url)}
-      >
-        <Card
-          featuredTitle={title}
-          featuredTitleStyle={featuredTitleStyle}
-          image={{
-            uri: urlToImage || defaultImg
-          }}
-        >
-          <Text style={{ marginBottom: 10 }}>
-            {description || 'Read More..'}
-          </Text>
-          <Divider style={{ backgroundColor: '#dfe6e9' }} />
-          <View
-            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
-          >
-            <Text style={noteStyle}>{source.name.toUpperCase()}</Text>
-            <Text style={noteStyle}>{time}</Text>
-          </View>
-        </Card>
-      </TouchableNativeFeedback>
-    );
-  }
+        // destructuring variable "article" passed from App.js
+        const {
+            title, description, publishedAt, source, urlToImage, url
+        } = this.props.articles;
+
+        const { noteStyle, featuredTitleStyle } = styles;
+        const time = moment(publishedAt || moment.now()).fromNow();
+        const defaultImg = "hhttps://wallpaper.wiki/wp-content/uploads/2017/04/wallpaper.wiki-Images-HD-Diamond-Pattern-PIC-WPB009691.jpg";
+
+        return (
+            <TouchableNativeFeedback
+
+                // tell the element to use the foreground when displaying the ripple effect on the card
+                useForeground
+                onPress = {() => Linking.openURL(url)}
+            >
+
+                <Card
+                    featuredTitle={title}
+                    featuredTitleStyle={featuredTitleStyle}
+                    image={{
+
+                        // if urlToImage is null, use defaultImg
+                        url: urlToImage || defaultImg
+                    }}
+                >
+
+                    <Text style={{ marginBottom: 10 }}>
+                        {description || 'Read More...'}
+                    </Text>
+
+                    <Divider style={{ backgroundColor: '#dfe6e9'}} />
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Text style={noteStyle}>{source.name.toUpperCase()}</Text>
+                        <Text style={noteStyle}>{time}</Text>
+                    </View>
+                </Card>
+            </TouchableNativeFeedback>
+        );
+    }
 }
 
 const styles = {
-  noteStyle: {
-    margin: 5,
-    fontStyle: 'italic',
-    color: '#b2bec3',
-    fontSize: 10
-  },
-  featuredTitleStyle: {
-    marginHorizontal: 5,
-    textShadowColor: '#00000f',
-    textShadowOffset: { width: 3, height: 3 },
-    textShadowRadius: 3
-  }
+    noteStyle: {
+        margin: 5,
+        fontStyle: 'italic',
+        color: '#b2bec3',
+        fontSize: 10
+    }, 
+    featuredTitleStyle: {
+        marginHorizontal: 5,
+        textShadowColor: '#0000f',
+        textShadowOffset: { width: 3, height: 3 },
+        textShadowRadius: 3
+    }
 };
